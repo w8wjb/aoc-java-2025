6d5392017e1fd749cd0b5c8aafb1f8c735fbfc58
Weston Bustraan <wbustraan@gmail.com>
2025-12-11 08:57:49 -0500

    Day 9, second star finally!

--- src/main/java/org/tot/aoc/Day9.java
+++ src/main/java/org/tot/aoc/Day9.java
@@ -3,15 +3,12 @@ package org.tot.aoc;
 import org.tot.aoc.grid.Point;
 
 import java.awt.geom.Line2D;
-import java.awt.geom.Point2D;
-import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Day9 {
 
     List<Point> points = new ArrayList<>();
-    List<Line> lines = new ArrayList<>();
 
     public long solvePuzzle1(List<String> input) {
 
@@ -61,63 +58,39 @@ public class Day9 {
 
     private static class Rectangle {
 
-        final Point cornerA;
-        final Point cornerB;
+        final Point tileA;
+        final Point tileB;
 
-        final long minX;
-        final long maxX;
-        final long minY;
-        final long maxY;
-        final long height;
-        final long width;
+        final long minX; // left
+        final long maxX; // right
+        final long minY; // top
+        final long maxY; // bottom
+        public final long width;
+        public final long height;
 
-        public Rectangle(Point cornerA, Point cornerB) {
-            this.cornerA = cornerA;
-            this.cornerB = cornerB;
-            minX = Math.min(cornerA.x, cornerB.x);
-            maxX = Math.max(cornerA.x, cornerB.x);
-            minY = Math.min(cornerA.y, cornerB.y);
-            maxY = Math.max(cornerA.y, cornerB.y);
+        public Rectangle(Point a, Point b) {
+
+            if (a.x < b.x) {
+                tileA = a;
+                tileB = b;
+            } else {
+                tileA = b;
+                tileB = a;
+            }
+
+            minX = Math.min(tileA.x, tileB.x);
+            maxX = Math.max(tileA.x, tileB.x);
+            minY = Math.min(tileA.y, tileB.y);
+            maxY = Math.max(tileA.y, tileB.y);
             this.height = maxY - minY;
             this.width = maxX - minX;
         }
 
-        boolean isInside(Point p) {
-            return p.x > minX && p.x < maxX &&
-                    p.y > minY && p.y < maxY;
-        }
-
-        boolean intersects(Line line) {
-            // If either endpoint is inside the rectangle, we count that as intersection
-            if (isInside(line.a) || isInside(line.b)) {
-                return true;
-            }
-
-            // W
-            if (Line2D.linesIntersect(this.minX, this.minY, this.minX, this.maxY,
-                    line.a.x, line.a.y, line.b.x, line.b.y)) {
-                return true;
-            }
-
-            // E
-            if (Line2D.linesIntersect(this.maxX, this.minY, this.maxX, this.maxY,
-                    line.a.x, line.a.y, line.b.x, line.b.y)) {
-                return true;
-            }
-
-            // N
-            if (Line2D.linesIntersect(this.minX, this.minY, this.maxX, this.minY,
-                    line.a.x, line.a.y, line.b.x, line.b.y)) {
-                return true;
-            }
-
-            // S
-            if (Line2D.linesIntersect(this.minX, this.maxY, this.maxX, this.maxY,
-                    line.a.x, line.a.y, line.b.x, line.b.y)) {
-                return true;
-            }
-
-            return false;
+        public boolean intersects(Rectangle other) {
+            return this.minX < other.maxX &&
+                    this.maxX > other.minX &&
+                    this.minY < other.maxY &&
+                    this.maxY > other.minY;
         }
 
         long area() {
@@ -129,7 +102,9 @@ public class Day9 {
 
     public long solvePuzzle2(List<String> input) {
 
-        Point last = null;
+        Point loopStart = new Point(0, Long.MAX_VALUE);
+        int loopStartIndex = -1;
+        List<Rectangle> whitespace = new ArrayList<>();
 
         long boundsMinX = Long.MAX_VALUE;
         long boundsMaxX = 0;
@@ -149,19 +124,35 @@ public class Day9 {
             Point p = new Point(x, y);
             points.add(p);
 
-            if (last == null) {
-                last = p;
-            } else {
-                lines.add(new Line(last, p));
+            if (p.y <= loopStart.y) {
+                loopStart = p;
+                loopStartIndex = points.size() - 1;
             }
         }
-        lines.add(new Line(last, points.get(0)));
+
+        for (int offset = 0; offset < points.size(); offset++) {
+            int i = (loopStartIndex + offset) % points.size();
+            int iNext = (loopStartIndex + offset + 1) % points.size();
+
+            Point p1 = points.get(i);
+            Point p2 = points.get(iNext);
+
+            if (p1.x == p2.x) { // vertical
+                Point nw;
+                Point se;
+                if (p1.y < p2.y) { // down
+                    nw = new Point(p1.x, p1.y);
+                    se = new Point(boundsMaxX, p2.y);
+                } else { // up
+                    nw = new Point(boundsMinX, p2.y);
+                    se = new Point(p1.x, p1.y);
+                }
+                whitespace.add(new Rectangle(nw, se));
+            }
+        }
+
 
         long maxArea = 0;
-
-        Point lastA = null;
-        Point lastB = null;
-        Rectangle largestRectangle = null;
 
         for (int i = 0; i < points.size(); i++) {
             Point p1 = points.get(i);
@@ -169,52 +160,25 @@ public class Day9 {
                 Point p2 = points.get(j);
 
                 Rectangle rect = new Rectangle(p1, p2);
-
-                long area = rect.area();
-                if (area <= maxArea) {
+                if (rect.area() < maxArea) {
                     continue;
                 }
 
-                if (isValid(rect, p1, p2)) {
-                    maxArea = Math.max(maxArea, area);
-                    lastA = p1;
-                    lastB = p2;
-                    largestRectangle = rect;
+                boolean valid = true;
+                for (Rectangle r : whitespace) {
+                    if (rect.intersects(r)) {
+                        valid = false;
+                        break;
+                    }
+                }
+
+                if (valid) {
+                    maxArea = Math.max(maxArea, rect.area());
                 }
             }
         }
 
-
-        if (largestRectangle != null) {
-            System.out.printf("%d, %d %d,%d%n", largestRectangle.minX, largestRectangle.minY, largestRectangle.width, largestRectangle.height);
-        }
-
-
         return maxArea;
-
-    }
-
-    boolean isValid(Rectangle rect, Point p1, Point p2) {
-        for (Point p : points) {
-            if (rect.isInside(p)) {
-                return false;
-            }
-        }
-
-
-        for (Line line : lines) {
-            if (line.a.equals(p1)
-                    || line.b.equals(p1)
-                    || line.a.equals(p2)
-                    || line.b.equals(p2)) {
-                continue;
-            }
-
-            if (rect.intersects(line)) {
-                return false;
-            }
-        }
-        return true;
     }
 
 
